6b4e2cbcd948fc9a380ad846fd78a70ce922f776
fmonsalveusa <#Camila18>
2026-03-30 15:18:29 -0400

    Update bundle ID to com.dispatchup.driver2

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.dispatchup.driver',
+  appId: 'com.dispatchup.driver2',
   appName: 'Dispatch Up Driver',
   webDir: 'dist',
   android: {
