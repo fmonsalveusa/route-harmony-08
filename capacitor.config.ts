d811346e95aebdd134c4c93354ae0a5dfe341bb9
fmonsalveusa <#Camila18>
2026-04-06 15:02:37 -0400

    Add dispatch-up.com URL for Android

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,17 +1,23 @@
 import type { CapacitorConfig } from '@capacitor/cli';
-
 const config: CapacitorConfig = {
   appId: 'com.dispatchup.driver2',
-  appName: 'Dispatch Up',
+  appName: 'Dispatch Up Driver',
   webDir: 'dist',
+  android: {
+    useLegacyBridge: true,
+  },
+  server: {
+    url: 'https://www.dispatch-up.com',
+    cleartext: true,
+  },
   plugins: {
     SplashScreen: {
       launchAutoHide: true,
+      androidScaleType: 'CENTER_CROP',
     },
     Camera: {
       presentationStyle: 'fullscreen',
     },
   },
 };
-
 export default config;
